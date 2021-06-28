14f97df7e712b2e1d5b834272658013171b8d7b0
nathaniel-alvin <alvinparali@gmail.com>
2021-06-28 08:49:26 +0700

    Add Source File

---
+++ src/com/nathaniel/AccountNumber.java
@@ -0,0 +1,43 @@
+package com.nathaniel;
+
+import java.util.Random;
+
+public class AccountNumber {
+
+    private long accountNum;
+
+    // Constructor with known acc number
+    public AccountNumber(long accountNum) {this.accountNum = accountNum;}
+
+    // Constructor for new acc
+    // ID number is a 4 digit number
+    public AccountNumber(int ID) { accountNum = generateAccNum(ID); }
+
+    // Account number format:
+    // 5-digit number
+    // first 2 number = first 2 digit from IdNumber
+    // last 3 number = 3-digit random number
+    private long generateAccNum (int thisIdNumber ) {
+        Random random = new Random();
+
+        long tempAccNum = (long) ((thisIdNumber / 99) * Math.pow(10,2)); // first 2 digit from ID number
+        tempAccNum += 100 + random.nextInt(900); // random 3 digit number
+        return tempAccNum;
+    }
+
+    // verify account number from input
+     public boolean verifyAccNum (Long accountNum) { return this.accountNum == accountNum; }
+
+    public long getAccountNum () { return accountNum;}
+
+    public void showInfo() {
+        System.out.println("Account Number: " + accountNum);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(accountNum);
+        return sb.toString();
+    }
+}
---
+++ src/com/nathaniel/Profile.java
@@ -0,0 +1,99 @@
+package com.nathaniel;
+// class for profile
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Objects;
+
+public class Profile {
+    private String name;
+    private int ID; // 4-digit custom ID
+    private LinkedList<Long> accountNumberList;
+
+    public Profile(String name, int ID, LinkedList<Long> accountNumberList) {
+        this.name = name;
+        this.ID = ID;
+        this.accountNumberList = accountNumberList;
+    }
+
+    // check if name exist
+    public boolean verifyName(String nameAttempt) { return nameAttempt.equals(name); }
+
+    // check valid ID
+    public boolean verifyID(int ID) { return this.ID == ID; }
+
+    // Check if there is account for the profile
+    public boolean hasAccount() {
+        if (accountNumberList.isEmpty()) return false;
+        else return true;
+    }
+
+    // add account
+    public void addAcc(long accountNumber) {
+        accountNumberList.add(accountNumber);
+    }
+
+    // remove account
+    public void remAcc(long accountNumber) {
+        accountNumberList.remove(accountNumber);
+    }
+
+    // check if inputted accountNumber is in the profile
+    public boolean checkAccount(long accNumber) {
+        for (Iterator<Long> i = accountNumberList.iterator(); i.hasNext(); ) {
+            Long accountNumber = i.next();
+            if (accountNumber == accNumber) return true;
+        }
+        return false;
+    }
+
+    public void showAccounts() {
+        Long accountNumber;
+        int count = 1;
+
+        System.out.println("\nAccounts: ");
+        for (Iterator<Long> i = accountNumberList.iterator(); i.hasNext(); ) {
+            accountNumber = i.next();
+            System.out.println(count + ": " + accountNumber);
+            count++;
+        }
+    }
+    public void showInfo() {
+        Long accountNumber;
+
+        System.out.println("Name: " + name);
+        System.out.println("Account Numbers: ");
+        for (Iterator<Long> i = accountNumberList.iterator(); i.hasNext(); ) {
+            accountNumber = i.next();
+            System.out.println(accountNumber + ",");
+        }
+        System.out.println();
+    }
+
+    @Override
+    public String toString() {
+        Long accountNumber;
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(name).append(",").append(ID);
+        for (Iterator<Long> i = accountNumberList.iterator(); i.hasNext(); ) {
+            accountNumber = i.next();
+            sb.append(",").append(accountNumber);
+        }
+        return sb.toString();
+    }
+
+    //
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Profile profile = (Profile) o;
+        return ID == profile.ID && name.equals(profile.name) && accountNumberList.equals(profile.accountNumberList);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, ID, accountNumberList);
+    }
+}
