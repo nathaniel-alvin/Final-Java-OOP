f71ac0ca70e76cef8a69c1d56fb763c3abc0a8bb
nathaniel-alvin <75318949+nathaniel-alvin@users.noreply.github.com>
2021-06-28 19:36:37 +0700

    Delete src dir

--- src/com/nathaniel/Transaction.java
+++
@@ -1,7 +0,0 @@
-package com.nathaniel;
-
-public interface Transaction {
-    void deposit( double amount );
-    void withdraw( double amount );
-    void transfer(Account target, double amount );
-}
